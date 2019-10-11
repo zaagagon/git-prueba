4730786cab1b9b30466b1ae5ab66fbb0be06f147
zag <zaagagon@gmail.com>
2019-10-11 18:33:47 -0500

    separacion de las funciones basicas y archivo mate

--- programs/index.js
+++ programs/index.js
@@ -1,12 +1,6 @@
-//declaracion de funciones basicas
-function sumar() {
 
-}
 
-function restar (){
-
-}
-
-function multiplicar(){
-    
-}
+console.log(sumar(34,8));
+console.log(multiplicar(10,8));
+console.log(dividir(360,4));
+console.log(restar(120,75));
